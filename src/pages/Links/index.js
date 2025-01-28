0b282fa54b360f81fa1bf82257c056619103d6ae
Micael <micaelxv1@gmail.com>
2025-01-27 21:23:01 -0300

    home finalizada

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,7 +1,25 @@
+import './links.css';
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
+
 export default function Links() {
   return (
-    <div>
-      <h1>Sujeito Link!</h1>
+    <div className='links-container'>
+      
+      <div className="links-header">
+        <FiArrowLeft size={38} color="#fff" />
+        <h1>Meus Links</h1>
+      </div>
+
+      <div className="links-items">
+        <button className="link">
+          <FiLink size={18} color="#fff" />
+          https://rocketseat.com.br
+        </button>
+        <button>
+          <FiTrash size={18} color="#FF55454" />
+        </button>
+      </div>
+
     </div>
   );
 }
