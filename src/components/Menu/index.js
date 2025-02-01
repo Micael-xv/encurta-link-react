c3b2fcf309963a05ec885d331021042eed2a4c0e
Micael <micaelxv1@gmail.com>
2025-01-31 21:53:50 -0300

    interface completa

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Menu() {
   return (
     <div className="menu">
-      <a className="social" href="https://www.instagram.com/micael_trevisan">
+      <a className="social" target="_blank" href="https://www.instagram.com/micael_trevisan">
         <BsInstagram size={24} color="#FFF" /> 
       </a>
       <Link className="menu-item" to="/links">
